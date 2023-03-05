032c21cadcbe940b2c449e193052f43c12e69595
Nicolas Tesla <65693466+NicolasTeslaa@users.noreply.github.com>
2023-03-05 16:14:11 -0300

    atts

---
+++ appMaui/appFinanceiro/appFinanceiro/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace appFinanceiro;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ appMaui/appFinanceiro/appFinanceiro/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace appFinanceiro;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
